5119c0844b7c264dc85747c33fc8edaaa3e08121
henry440 <440henry@gmail.com>
2020-10-20 17:11:33 +0200

    Add Logoutputs to Json Parser

--- jsonParser.py
+++ jsonParser.py
@@ -7,6 +7,7 @@ class parser():
 
     def __init__(self, content, name):
         self.LOGGER = logger(f"LG {name}")
+        self.LOGGER.log("Parser wird gestartet", 1)
         self.name = name
         self.content = content["lookbehind"] #Alle Daten einer Station 
         self.zuege = []                      #Beinhaltet Daten sortiert nach zug
@@ -14,12 +15,17 @@ class parser():
             for i in self.content:
                 self.zuege.append(i)
         except Exception as e:
-            self.LOGGER.log("Harter Fehler", 3)
-            self.LOGGER.log(str(e))
+            try:
+                self.zuege.append(self.content[0])
+            except Exception as fat:
+                self.LOGGER.log("Harter Fehler", 3)
+                self.LOGGER.log(str(e),3)
+                self.LOGGER.log(str(fat),3)
         self.LOGGER.log(f"Gefundene Abfahrten/Ankünfte {len(self.zuege)}",1)
         
 
     def fixMissing(self, tag):
+        self.LOGGER.log("Fix Fehlende einträge", 0)
         ret = []
         for i in range(len(tag[1])):
             ret.append("NA")
@@ -50,4 +56,5 @@ class parser():
                 for dat in datas:
                     zeile.append(dat)
             out.append(zeile)
+        self.LOGGER.log("Verarbeitung abgeschlossen")
         return out
